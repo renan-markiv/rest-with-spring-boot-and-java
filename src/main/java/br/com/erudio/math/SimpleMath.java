f439f068e81d7fd0f6ebdf4746268edb3d114b0f
Renan Markiv da Silva <renan.markiv100@gmail.com>
2024-08-13 11:02:53 -0300

    testes matematicos

---
+++ src/main/java/br/com/erudio/math/SimpleMath.java
@@ -0,0 +1,31 @@
+package br.com.erudio.math;
+
+public class SimpleMath {
+
+    public Double sum(Double numberOne, Double numberTwo)
+    {
+
+        return numberOne + numberTwo;
+    }
+
+    public Double sub(Double numberOne, Double numberTwo)
+    {
+        return numberOne - numberTwo;
+    }
+
+    public Double mult(Double numberOne, Double numberTwo)
+    {
+        return numberOne * numberTwo;
+    }
+
+    public Double div(Double numberOne, Double numberTwo)
+    {
+
+        return numberOne / numberTwo;
+    }
+
+    public Double avg(Double numberOne, Double numberTwo)
+    {
+        return (numberOne + numberTwo) / 2;
+    }
+}
